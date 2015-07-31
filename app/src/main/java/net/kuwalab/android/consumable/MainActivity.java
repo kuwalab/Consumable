efd80e2112594b0cd8a0fe21e9a969824ea3e8c8
kuwalab <arata.shinichi@gmail.com>
2015-07-31 09:25:40 +0900

    ライセンスの表記を追加

--- app/src/main/java/net/kuwalab/android/consumable/MainActivity.java
+++ app/src/main/java/net/kuwalab/android/consumable/MainActivity.java
@@ -1,13 +1,17 @@
 package net.kuwalab.android.consumable;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ListView;
 
 import net.kuwalab.android.consumable.dao.ConsumableDao;
@@ -67,8 +71,27 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_lisence) {
+            final Dialog dialog = new Dialog(this);
+            dialog.setContentView(R.layout.webview_lisence);
+            dialog.setTitle("オープンソースライセンス");
+            dialog.setCancelable(true);
+            WebView webview = (WebView)dialog.findViewById(R.id.webview);
+
+            webview.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+            webview.setWebViewClient(new WebViewClient() {
+                @Override
+                public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                    super.onPageStarted(view, url, favicon);
+                }
+
+                @Override
+                public void onPageFinished(WebView view, String url) {
+                    super.onPageFinished(view, url);
+                    dialog.show();
+                }
+            });
+            webview.loadUrl("file:///android_asset/lisence.html");
         }
 
         return super.onOptionsItemSelected(item);
--- app/src/main/java/net/kuwalab/android/consumable/NewActivity.java
+++ app/src/main/java/net/kuwalab/android/consumable/NewActivity.java
@@ -180,7 +180,7 @@ public class NewActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_lisence) {
             return true;
         }
 
